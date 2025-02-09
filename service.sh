208d2593458def13277348553451a0f9f150ffee
Pavel Horniak <35545900+Gouster4@users.noreply.github.com>
2025-02-09 07:37:50 +0100

    Update service.sh
    Auto add TrickyStore security_patch.txt

--- service.sh
+++ service.sh
@@ -1,2 +1,6 @@
 #!/system/bin/sh
+MODDIR=${0%/*}
 resetprop -n ro.vendor.build.security_patch $(getprop ro.build.version.security_patch)
+if [[ -d /data/adb/tricky_store && ! -f /data/adb/tricky_store/security_patch.txt ]]; then
+cp $MODDIR/security_patch.txt /data/adb/tricky_store/security_patch.txt
+fi
